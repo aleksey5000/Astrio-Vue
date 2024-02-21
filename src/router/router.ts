bf34d596d9d5392619545d63af178d67d7e118c4
aleksey5000 <myshov.aleksey@mail.ru>
2024-02-21 14:47:52 +0300

    v1

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,11 +1,11 @@
-import MainPage from "../components/MainPage/MainPage.vue";
+import App from "../App.vue";
 import Basket from "../components/BasketComponent/Basket.vue";
 import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
   {
     path: "/",
-    component: MainPage,
+    component: App,
   },
   {
     path: "/basket",
